54cfc53745d1ebd92cc89fa8b9afafe0f7362f28
William Lety <william.lety@gmail.com>
2023-05-22 23:28:55 +0200

    First commit

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -6,16 +6,29 @@ use Fig\Http\Message\RequestMethodInterface as RequestMethod;
 use Pg\Router\Exception\ImmutableProperty;
 use Pg\Router\Exception\InvalidArgumentException;
 use Pg\Router\Route;
+use Pg\Router\RouteCollector;
+use Pg\Router\RouteGroup;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 
 class RouteTest extends TestCase
 {
     protected string $callback;
 
+    /**
+     */
     protected function setUp(): void
     {
         parent::setUp();
         $this->callback = 'MonController::method';
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function getCollector()
+    {
+        return $this->createMock(RouteCollector::class);
     }
 
     public function testRouteGetPath(): void
@@ -101,5 +114,42 @@ class RouteTest extends TestCase
         $this->expectExceptionMessage('Http methods array is empty');
 
         new Route('/foo', $this->callback, 'test', []);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRouteGetRouteGroup()
+    {
+        $route = new Route('/prefix/foo', $this->callback);
+
+        $routeGroup = new RouteGroup(
+            '/prefix',
+            fn(RouteGroup $routeGroup) => $routeGroup,
+            $this->getCollector()
+        );
+
+        $route->setParentGroup($routeGroup);
+        $this->assertSame($routeGroup, $route->getParentGroup());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRouteWithRouteGroupPrefix()
+    {
+        $collector = $this->getCollector();
+        $collector->expects($this->once())
+            ->method('route')
+            ->willReturn(new Route('/prefix/foo', $this->callback));
+
+        $routeGroup = new RouteGroup(
+            '/prefix',
+            fn(RouteGroup $routeGroup) => $routeGroup,
+            $collector
+        );
+
+        $route = $routeGroup->route('/foo', $this->callback, 'test');
+        $this->assertSame('/prefix/foo', $route->getPath());
     }
 }
