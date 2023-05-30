f79b0823b98805afaf37f306f37f3cc428021308
William Lety <william.lety@gmail.com>
2023-05-30 23:09:00 +0200

    First commit

--- src/Generator/GeneratorInterface.php
+++ src/Generator/GeneratorInterface.php
@@ -2,7 +2,19 @@
 
 namespace Pg\Router\Generator;
 
+use Exception;
+use Pg\Router\Exception\RouteNotFoundException;
+
 interface GeneratorInterface
 {
+    /**
+     * Generate a URI from the named route.
+     *
+     * Takes the named route and any substitutions, and attempts to generate a
+     * URI from it.
+     * Variable attributes must be given. Optionals attributes could be omitted.
+     *
+     * @throws Exception|RouteNotFoundException If unable to generate the given URI.
+     */
     public function generate(string $name, array $attributes = []): string;
 }
