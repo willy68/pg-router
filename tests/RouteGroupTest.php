0503b72784bf09f8273bcf82f1d47776067c0a48
willy <william.lety@gmail.com>
2025-05-27 20:07:38 +0200

    First commit

---
+++ tests/RouteGroupTest.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PgTest\Router;
+
+use Pg\Router\RouteGroup;
+use Pg\Router\RouteCollectionInterface;
+use Pg\Router\RouterInterface;
+use Pg\Router\Route;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+
+class RouteGroupTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testConstructorSetsProperties()
+    {
+        /** @var RouteCollectionInterface $router */
+        $router = $this->createMock(RouteCollectionInterface::class);
+        $callback = function () {
+        };
+        $group = new RouteGroup('/prefix', $callback, $router);
+
+        $this->assertSame('/prefix', $group->getPrefix());
+        $this->assertInstanceOf(RouteGroup::class, $group);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetPrefixReturnsPrefix()
+    {
+        /** @var RouteCollectionInterface $router */
+        $router = $this->createMock(RouteCollectionInterface::class);
+        $group = new RouteGroup('/admin', function () {
+        }, $router);
+        $this->assertEquals('/admin', $group->getPrefix());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetPrefix()
+    {
+        /** @var RouterInterface $router */
+        $router = $this->createMock(RouterInterface::class);
+        $group = new RouteGroup('/admin', function () {
+        }, $router);
+        $this->assertSame('/admin', $group->getPrefix());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testInvokeCallsCallback()
+    {
+        /** @var RouterInterface $router */
+        $router = $this->createMock(RouterInterface::class);
+        $called = false;
+        $group = new RouteGroup('/admin', function ($arg) use (&$called) {
+            $called = true;
+            $this->assertInstanceOf(RouteGroup::class, $arg);
+        }, $router);
+        $group();
+        $this->assertTrue($called);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRouteAddsPrefixAndSetsParentGroup()
+    {
+        $router = $this->createMock(RouterInterface::class);
+        $route = $this->createMock(Route::class);
+
+        $router->expects($this->once())
+            ->method('route')
+            ->with('/admin/foo', 'cb', 'name', ['GET'])
+            ->willReturn($route);
+
+        $route->expects($this->once())
+            ->method('setParentGroup')
+            ->with($this->isInstanceOf(RouteGroup::class));
+
+        /** @var RouterInterface $router */
+        $group = new RouteGroup('/admin', function () {
+        }, $router);
+        $result = $group->route('/foo', 'cb', 'name', ['GET']);
+        $this->assertSame($route, $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRouteWithSlashPath()
+    {
+        $router = $this->createMock(RouterInterface::class);
+        $route = $this->createMock(Route::class);
+
+        $router->expects($this->once())
+            ->method('route')
+            ->with('/prefix', 'cb', null, null)
+            ->willReturn($route);
+
+        $route->expects($this->once())
+            ->method('setParentGroup')
+            ->with($this->isInstanceOf(RouteGroup::class));
+
+        /** @var RouterInterface $router */
+        $group = new RouteGroup('/prefix', function () {
+        }, $router);
+        $result = $group->route('/', 'cb');
+        $this->assertSame($route, $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testCrudCallsVerbMethodsAndReturnsSelf()
+    {
+        $group = $this->getMockBuilder(RouteGroup::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['get', 'post', 'delete'])
+            ->getMock();
+        $route = $this->createMock(Route::class);
+
+        $expected1 = ['/', 'UserController::index', 'user.index'];
+        $expected2 = ['/new', 'UserController::create', 'user.create'];
+        $matcher = $this->exactly(2);
+        $group->expects($matcher)
+            ->method('get')
+            ->willReturnCallback(
+                function (string $path, string $callback, string $name) use ($matcher, $expected1, $expected2) {
+                    match ($matcher->numberOfInvocations()) {
+                        1 => $this->assertSame($expected1, [$path, $callback, $name]),
+                        2 => $this->assertSame($expected2, [$path, $callback, $name]),
+                    };
+                }
+            )
+            ->willReturn($route);
+
+        $expected1 = ['/new', 'UserController::create', 'user.create.post'];
+        $expected2 = ['/{id:\d+}', 'UserController::edit', 'user.edit.post'];
+        $matcher = $this->exactly(2);
+        $group->expects($matcher)
+            ->method('post')
+            ->willReturnCallback(
+                function (string $path, string $callback, string $name) use ($matcher, $expected1, $expected2) {
+                    match ($matcher->numberOfInvocations()) {
+                        1 => $this->assertSame($expected1, [$path, $callback, $name]),
+                        2 => $this->assertSame($expected2, [$path, $callback, $name]),
+                    };
+                }
+            )
+            ->willReturn($route);
+
+        $group->expects($this->once())
+            ->method('delete')
+            ->with('/{id:\d+}', 'UserController::delete', 'user.delete')
+            ->willReturn($route);
+
+        /** @var RouteGroup $group */
+        $result = $group->crud('UserController', 'user');
+        $this->assertSame($group, $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testCrudAddsAllCrudRoutes()
+    {
+        $router = $this->createMock(RouterInterface::class);
+        $route = $this->createMock(Route::class);
+
+        $router->method('route')->willReturn($route);
+        $route->method('setParentGroup')->willReturnSelf();
+
+        $group = $this->getMockBuilder(RouteGroup::class)
+            ->setConstructorArgs(['/users', function () {
+            }, $router])
+            ->onlyMethods(['get', 'post', 'delete'])
+            ->getMock();
+
+        $group->expects($this->exactly(2))->method('get');
+        $group->expects($this->exactly(2))->method('post');
+        $group->expects($this->once())->method('delete');
+
+        /** @var RouteGroup $group */
+        $result = $group->crud('UserController', 'user');
+        $this->assertSame($group, $result);
+    }
+}
--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -6,8 +6,17 @@ use Exception;
 use GuzzleHttp\Psr7\ServerRequest;
 use Pg\Router\Exception\MissingAttributeException;
 use Pg\Router\Exception\RouteNotFoundException;
+use Pg\Router\RegexCollector\RegexCollectorInterface;
+use Pg\Router\Route;
 use Pg\Router\Router;
 use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheException;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Cache\InvalidArgumentException;
+use ReflectionClass;
+use ReflectionException;
+use RuntimeException;
 
 class RouterTest extends TestCase
 {
@@ -46,7 +55,7 @@ class RouterTest extends TestCase
         $router->generateUri('test.uri');
 
         // test with bad attribute
-        $this->expectException(\RuntimeException::class);
+        $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage(sprintf(
             'Parameter value for [%s] did not match the regex `%s`',
             'id',
@@ -55,6 +64,9 @@ class RouterTest extends TestCase
         $router->generateUri('test.uri', ['id' => 'slug']);
     }
 
+    /**
+     * @throws InvalidArgumentException
+     */
     public function testMatchSuccess(): void
     {
         $router = new Router();
@@ -68,6 +80,9 @@ class RouterTest extends TestCase
         $this->assertSame(['name' => 'john'], $result->getMatchedAttributes());
     }
 
+    /**
+     * @throws InvalidArgumentException
+     */
     public function testMatchFailureMethodNotAllowed(): void
     {
         $router = new Router();
@@ -84,8 +99,160 @@ class RouterTest extends TestCase
     public function testCrudRoutes(): void
     {
         $router = new Router();
-        $group = $router->crud('/users', 'UserController', 'user');
+        $router->crud('/users', 'UserController', 'user');
 
         $this->assertNotEmpty($router->getRoutes());
+    }
+
+    /**
+     * @throws CacheException
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws ReflectionException
+     */
+    public function testCacheIsUsedWhenEnabledAndHit(): void
+    {
+        $cachedData = ['some' => 'cached_data'];
+
+        $cacheItem = $this->createMock(CacheItemInterface::class);
+        $cacheItem->method('isHit')->willReturn(true);
+        $cacheItem->method('get')->willReturn($cachedData);
+
+        $cachePool = $this->createMock(CacheItemPoolInterface::class);
+        $cachePool->method('getItem')->willReturn($cacheItem);
+
+        $router = new Router(null, null, [
+            Router::CONFIG_CACHE_ENABLED => true,
+            Router::CONFIG_CACHE_FILE => '/tmp/cache_file.php'
+        ]);
+
+        $reflection = new ReflectionClass($router);
+        $property = $reflection->getProperty('cachePool');
+        $property->setAccessible(true);
+        $property->setValue($router, $cachePool);
+
+        $data = $this->invokeParsedData($router);
+
+        $this->assertSame($cachedData, $data);
+    }
+
+    /**
+     * @throws CacheException
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws ReflectionException
+     */
+    public function testCacheIsSavedWhenMiss(): void
+    {
+        $route = new Route('/test', 'callback', 'test');
+
+        $regexCollector = $this->createMock(RegexCollectorInterface::class);
+        $regexCollector->expects($this->once())->method('addRoute');
+        $regexCollector->method('getData')->willReturn(['parsed' => 'data']);
+
+        $cacheItem = $this->createMock(CacheItemInterface::class);
+        $cacheItem->method('isHit')->willReturn(false);
+        $cacheItem->expects($this->once())->method('set')->with(['parsed' => 'data']);
+
+        $cachePool = $this->createMock(CacheItemPoolInterface::class);
+        $cachePool->method('getItem')->willReturn($cacheItem);
+        $cachePool->expects($this->once())->method('save')->with($cacheItem);
+
+        $router = new Router($regexCollector, null, [
+            Router::CONFIG_CACHE_ENABLED => true,
+            Router::CONFIG_CACHE_FILE => '/tmp/cache_file.php'
+        ]);
+        $router->addRoute($route);
+
+        $reflection = new ReflectionClass($router);
+        $property = $reflection->getProperty('cachePool');
+        $property->setAccessible(true);
+        $property->setValue($router, $cachePool);
+
+        $this->invokeParsedData($router);
+    }
+
+    /**
+     * @throws CacheException
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws ReflectionException
+     */
+    public function testNoCacheWhenDisabled(): void
+    {
+        $regexCollector = $this->createMock(RegexCollectorInterface::class);
+        $regexCollector->expects($this->once())->method('getData')->willReturn(['fresh' => 'data']);
+
+        $router = new Router($regexCollector, null, [
+            Router::CONFIG_CACHE_ENABLED => false
+        ]);
+
+        $route = new Route('/test', 'callback', 'test');
+        $router->addRoute($route);
+
+        $data = $this->invokeParsedData($router);
+
+        $this->assertEquals(['fresh' => 'data'], $data);
+    }
+
+    /**
+     * @throws CacheException
+     * @throws InvalidArgumentException
+     */
+    public function testCacheStoresAndRestoresParsedData(): void
+    {
+        $tmpDir = sys_get_temp_dir() . '/router_cache_' . uniqid();
+        mkdir($tmpDir);
+        $cacheFile = $tmpDir . '/router_cache.php';
+
+        $router = new Router(
+            null,
+            null,
+            [
+            Router::CONFIG_CACHE_ENABLED => true,
+            Router::CONFIG_CACHE_FILE => $tmpDir,
+            ]
+        );
+
+        $router->route('/cache-test/{id:\d+}', 'TestController::cache', 'cache.route', ['GET']);
+
+    // First match to populate cache
+        $request = new \GuzzleHttp\Psr7\ServerRequest('GET', '/cache-test/123');
+        $result = $router->match($request);
+
+        $this->assertTrue($result->isSuccess());
+        $this->assertSame('cache.route', $result->getMatchedRouteName());
+        $this->assertSame(['id' => '123'], $result->getMatchedAttributes());
+
+    // Simulate a new Router instance with same cache config
+        $router2 = new Router(
+            null,
+            null,
+            [
+            Router::CONFIG_CACHE_ENABLED => true,
+            Router::CONFIG_CACHE_FILE => $tmpDir,
+            ]
+        );
+    // Add the same route (simulate fresh boot, but cache should be hit)
+        $router2->route('/cache-test/{id:\d+}', 'TestController::cache', 'cache.route', ['GET']);
+
+        $request2 = new \GuzzleHttp\Psr7\ServerRequest('GET', '/cache-test/456');
+        $result2 = $router2->match($request2);
+
+        $this->assertTrue($result2->isSuccess());
+        $this->assertSame('cache.route', $result2->getMatchedRouteName());
+        $this->assertSame(['id' => '456'], $result2->getMatchedAttributes());
+
+    // Clean up
+        array_map('unlink', glob($tmpDir . '/*'));
+        rmdir($tmpDir);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function invokeParsedData(Router $router): array
+    {
+        $reflection = new ReflectionClass($router);
+        $method = $reflection->getMethod('getParsedData');
+        $method->setAccessible(true);
+        return $method->invoke($router);
     }
 }
